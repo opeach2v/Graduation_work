7d8b45de6f1e39d76bd2039f0d7e7c245788fe20
opeach2v <opeach2v@naver.com>
2025-03-14 04:21:24 +0900

    DBtest

--- usersDB/models.py
+++ usersDB/models.py
@@ -8,3 +8,7 @@ class teachers(models.Model):
     ID = models.CharField(max_length=30)
     password = models.CharField(max_length=50)
     classNum = models.IntegerField(default=0)
+
+class testData(models.Model):
+    name = models.CharField(max_length=100)
+    ID = models.CharField(max_length=50)
