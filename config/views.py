e83ce2b3a3d5e7d4bcd0b65354a14d762780a4c8
opeach2v <opeach2v@naver.com>
2025-03-20 23:36:21 +0900

    teachers UI 2

--- config/views.py
+++ config/views.py
@@ -6,3 +6,8 @@ def main(request):
 def dbtest(request):
     return render(request, 'db_test.html')
 
+def parentsPage(request):
+    return render(request, 'parents_page.html')
+
+def teachersPage(request):
+    return render(request, 'teachers_page.html')
