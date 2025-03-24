4d1b5d6f039f1eec952e5e2c39f769a967a280ef
master <opeach2v@naver.com>
2025-03-24 12:20:24 +0900

    settings.py reupdate

--- config/settings.py
+++ config/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
 
