bd679d4919f91071d66e94d8f9022a1dbcf5cc1f
master <opeach2v@naver.com>
2025-03-27 18:04:59 +0900

    views render url change2

--- graduation_work/urls.py
+++ graduation_work/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='login_user'),    # 로그인 페이지 및 로그인 처리
     path('signup/', views.add_users, name='add_users'),    # 회원가입 페이지 및 회원가입 처리
-    path('parents/', views.parents_page, name='parents_page'),    # 부모님 페이지
-    path('teachers/', views.teachers_page, name='teachers_page'),    # 선생님 페이지
+    path('parents/', views.parentsPage, name='parents_page'),    # 부모님 페이지
+    path('teachers/', views.teachersPage, name='teachers_page'),    # 선생님 페이지
     path('show/', views.show_users),
 ]
