74338677f960b3e36be1fff8ac57ea25115ad3de
master <opeach2v@naver.com>
2025-03-27 17:17:10 +0900

    urls change3

--- graduation_work/urls.py
+++ graduation_work/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('signup/', views.show_createId_page, name='signup_form'),  # 회원가입 페이지
     path('add/', views.add_users, name='add_users'),    # 회원가입 처리
     path('show/', views.show_users),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
