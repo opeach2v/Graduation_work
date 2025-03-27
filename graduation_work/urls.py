075a17b1872eab49426dab950a4024cb81c95e10
master <opeach2v@naver.com>
2025-03-27 17:32:11 +0900

    signup real finish?

--- graduation_work/urls.py
+++ graduation_work/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='login'),
-    path('signup/', views.add_users, name='add_users'),    # 회원가입 처리
+    path('signup/', views.add_users, name='signup'),    # 회원가입 처리
     path('show/', views.show_users),
 ]
