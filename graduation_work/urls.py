ab55b11fd12241f2cfda772e22fd907cd2c5205a
master <opeach2v@naver.com>
2025-03-27 18:17:48 +0900

    login rrreal finish

--- graduation_work/urls.py
+++ graduation_work/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='login_user'),    # 로그인 페이지 및 로그인 처리
+    path('', views.login_user, name='login_user'),    # 로그인 페이지 및 로그인 처리
     path('signup/', views.add_users, name='add_users'),    # 회원가입 페이지 및 회원가입 처리
     path('parents/', views.parentsPage, name='parents_page'),    # 부모님 페이지
     path('teachers/', views.teachersPage, name='teachers_page'),    # 선생님 페이지
