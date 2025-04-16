8ff6f256f6d25f7953c569f627889857722ba920
opeach2v <opech2v@naver.com>
2025-04-16 17:17:30 +0900

    video.

--- static/js/parents_page.js
+++ static/js/parents_page.js
@@ -1,10 +1,22 @@
-//팝업 띄우기
+// 알림장 팝업 띄우기
 function openPop() {
     document.getElementById("popup_layer").style.display = "block";
 
 }
 
-//팝업 닫기
+// 알림장 팝업 닫기
 function closePop() {
     document.getElementById("popup_layer").style.display = "none";
+}
+
+
+// 오늘의 하루 팝업 띄우기
+function openTodayPop() {
+    document.getElementById("today_popup_layer").style.display = "block";
+
+}
+
+// 오늘의 하루 팝업 닫기
+function closeTodayPop() {
+    document.getElementById("today_popup_layer").style.display = "none";
 }
