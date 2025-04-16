ca6e6df38e59b1079cad7cd07c3eb83e8c0d381e
opeach2v <opech2v@naver.com>
2025-04-16 18:53:53 +0900

    nav and header update

--- graduation_work/views.py
+++ graduation_work/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from .models import users_collection
 
+import bcrypt;
+
 # 로그인 페이지 (메인)
 def main(request):
     return render(request,'graduation_work/main.html')
@@ -21,10 +23,12 @@ def add_users(request):
             role = request.POST.get('role')
             name = request.POST.get('name')
 
+            hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt());
+
             # 데이터 생성
             data = {
                 "username": username,
-                "password": password,
+                "password": hashed_pw,  # 해시된 비번 저장
                 "role": role,
                 "name": name,
                 "createdAt": datetime.now()
@@ -36,7 +40,7 @@ def add_users(request):
         except Exception as e:
             return JsonResponse({"signup error" : str(e)}, status=500)
 
-     # GET 요청이 들어오면 회원가입 페이지를 렌더링
+    # GET 요청이 들어오면 회원가입 페이지를 렌더링
     return render(request, 'graduation_work/createId_page.html')
 
 # 로그인 처리
@@ -46,26 +50,33 @@ def login_user(request):
         try:
             # POST 데이터 받기
             username = request.POST.get('username')
-            password = request.POST.get('password')
+            input_password = request.POST.get('password')
             role = request.POST.get('role')
 
             # 데이터 조회 (username, password가 일치하는 데이터가 있는지 확인)
-            user_data = users_collection.find_one({"username": username, "password": password, "role": role})
+            user_data = users_collection.find_one({"username": username, "role": role})
             print(f"user_data: {user_data}")  # Debugging
             if user_data:
+                hashed_pw = user_data.get('password');
+
                 # 세션에 사용자 정보 수동으로 저장
-                request.session['username'] = user_data.get("username")
-                request.session['role'] = user_data.get("role")
-                request.session['name'] = user_data.get("name", [None])[0]
+                # 비밀번호 비교 (바이트로 변환)
+                if bcrypt.checkpw(input_password.encode('utf-8'), hashed_pw) :
+                    request.session['username'] = user_data.get("username")
+                    request.session['role'] = user_data.get("role")
+                    request.session['name'] = user_data.get("name", [None])[0]
                 
-                # role에 따라 리디렉션
-                if role == "parent":    # 부모님
-                    return redirect('parents_page')
-                elif role == "teacher": # 선생님
-                    return redirect('teachers_page')
+                    # role에 따라 리디렉션
+                    if role == "parent":    # 부모님
+                        return redirect('parents_page')
+                    elif role == "teacher": # 선생님
+                        return redirect('teachers_page')
+                    else:
+                        # 로그인 실패
+                        return render(request, 'graduation_work/main.html', {'error': '학부모와 선생님을 다시 선택해주세요.'})
                 else:
                     # 로그인 실패
-                    return render(request, 'graduation_work/main.html', {'error': '학부모와 선생님을 다시 선택해주세요.'})
+                    return render(request, 'graduation_work/main.html', {'error': '아이디 또는 비밀번호가 일치하지 않습니다.'})
             else:
                 # 로그인 실패
                 return render(request, 'graduation_work/main.html', {'error': '아이디 또는 비밀번호가 일치하지 않습니다.'})
